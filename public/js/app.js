566e3527daa45918d0a2afb85d920e15d17a299b
ronaldporch <ronaldporch@gmail.com>
2015-12-02 17:07:17 -0600

    Blah

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
-var app = angular.module('RealDemo', ['ui.router', 'angular-loading-bar', 'ui.bootstrap'])
-app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
+var app = angular.module('RealDemo', ['ui.router', 'angular-loading-bar', 'ui.bootstrap', 'uiGmapgoogle-maps'])
+app.config(['$urlRouterProvider', '$stateProvider', 'uiGmapGoogleMapApiProvider', function($urlRouterProvider, $stateProvider,uiGmapGoogleMapApiProvider){
 	$urlRouterProvider.otherwise('/')
 	$stateProvider
 		.state('home', {
@@ -7,14 +7,35 @@ app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider,
 			templateUrl: 'partials/home.html',
 			controller: 'HomeController'
 		})
+		.state('map', {
+			url: '/homes/:locale',
+			templateUrl: 'partials/map.html',
+			controller: 'MapController'
+		})
+	uiGmapGoogleMapApiProvider.configure({
+		v: '3.20',
+		libraries: 'weather, geometry, visualization'
+	});
 }])
-app.controller('HomeController', ['$scope', 'homes', function($scope, homes){
+app.controller('HomeController', ['$scope', 'homes', 'googleAPI', '$state', function($scope, homes, googleAPI, $state){
 	$scope.name = "Ronald"
 	$scope.address = ""
+	$scope.locations = function(val){
+		console.log(val)
+		return googleAPI.getLocations(val).then(function(res){
+			console.log(res.data)
+			return res.data.results.map(function(item){
+				return item.formatted_address;
+			})
+		}, function(err){
+			console.log(err)
+		})
+	}
 	$scope.getHomes = function(){
 		var address = $scope.address.replace(/ /g, "-");
 		address = address.replace(/,/g, "");
 		console.log(address)
+		$state.go('map', {locale: address})
 		homes.getHomes(address).then(
 			function(res){
 				console.log(res.data)
@@ -25,10 +46,35 @@ app.controller('HomeController', ['$scope', 'homes', function($scope, homes){
 			})
 	}
 }])
+app.controller('MapController', ['$scope', 'homes', 'googleAPI', '$stateParams', function($scope, homes, googleAPI, $stateParams){
+	$scope.locale = $stateParams.locale
+	$scope.getLocationInfo = function(){
+		var address = $scope.locale.replace(/-/g, " ");
+		googleAPI.getLocations(address).then(function(res){
+			console.log(res.data)
+		}, function(err){
+			console.log(err)
+		})
+	}
+	$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+	$scope.getLocationInfo()
+}])
 app.factory('homes', ['$http', function($http){
 	var o = {}
 	o.getHomes = function(address){
 		return $http.get('/api/homes/' + address)
+	}
+	return o
+}])
+app.factory('googleAPI', ['$http', function($http){
+	var o = {}
+	o.getLocations = function(val){
+		return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
+			params: {
+				address: val,
+				sensor: false
+			}
+		})
 	}
 	return o
 }])
